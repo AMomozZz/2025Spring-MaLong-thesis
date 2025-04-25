323c3fc3567f03b159a38d2a987a4354d9241422
Long Ma <malongjordanl@126.com>
2025-04-25 10:03:02 +0200

    q2,q3

--- host/run_wasm.bat
+++ host/run_wasm.bat
@@ -10,8 +10,16 @@ if %errorlevel% neq 0 (
 )
 
 echo Running with Wasmtime...
-wasmtime -O opt-level=0 -S cli=y -S inherit-network=y --dir ../nexmark-data ./target/wasm32-wasip2/release/host.wasm q1
-wasmtime -O opt-level=0 -S cli=y -S inherit-network=y --dir ../nexmark-data ./target/wasm32-wasip2/release/host.wasm q1-opt
-wasmtime -O opt-level=0 -S cli=y -S inherit-network=y --dir ../nexmark-data ./target/wasm32-wasip2/release/host.wasm io
+wasmtime -O opt-level=0 -S cli=y -S inherit-network=y --dir ../nexmark-data ./target/wasm32-wasip2/release/host.wasm ../nexmark-data/bid q1
+wasmtime -O opt-level=0 -S cli=y -S inherit-network=y --dir ../nexmark-data ./target/wasm32-wasip2/release/host.wasm ../nexmark-data/bid q1-opt
+
+wasmtime -O opt-level=0 -S cli=y -S inherit-network=y --dir ../nexmark-data ./target/wasm32-wasip2/release/host.wasm ../nexmark-data/bid q2
+wasmtime -O opt-level=0 -S cli=y -S inherit-network=y --dir ../nexmark-data ./target/wasm32-wasip2/release/host.wasm ../nexmark-data/bid q2-opt
+
+wasmtime -O opt-level=0 -S cli=y -S inherit-network=y --dir ../nexmark-data ./target/wasm32-wasip2/release/host.wasm ../nexmark-data/auctionPerson q3
+wasmtime -O opt-level=0 -S cli=y -S inherit-network=y --dir ../nexmark-data ./target/wasm32-wasip2/release/host.wasm ../nexmark-data/auctionPerson q3-opt
+
+wasmtime -O opt-level=0 -S cli=y -S inherit-network=y --dir ../nexmark-data ./target/wasm32-wasip2/release/host.wasm ../nexmark-data/bid io
+wasmtime -O opt-level=0 -S cli=y -S inherit-network=y --dir ../nexmark-data ./target/wasm32-wasip2/release/host.wasm ../nexmark-data/auctionPerson io
 @REM wasmtime -S inherit-network=y .\target\wasm32-wasip1\release\host.wasm 127 0 0 1 8080 127 0 0 1 8090 127 0 0 1 8100
 @REM wasmtime -S inherit-network=y ./main.wasm 127 0 0 1 8080 127 0 0 1 8090 127 0 0 1 8100
--- host/src/main.rs
+++ host/src/main.rs
@@ -1,9 +1,18 @@
 pub mod q1;
+pub mod q2;
+pub mod q3;
+// pub mod q4;
+// pub mod q5;
+// pub mod q6;
+// pub mod q7;
+// pub mod q8;
+// pub mod qw;
+
 pub mod data;
 
-// use crate::data::Auction;
+use crate::data::Auction;
 use crate::data::Bid;
-// use crate::data::Person;
+use crate::data::Person;
 
 use std::{fs::File, io::BufReader};
 use runtime::prelude::{serde::de::DeserializeOwned, *};
@@ -19,19 +28,69 @@ fn main() {
     let binding = environment::get_arguments();
     println!("{:?}", binding);
     let mut args = binding.iter().skip(1);
+    let Some(dir) = args.next() else {
+        println!("{USAGE}");
+        return;
+    };
     let Some(query) = args.next() else {
         println!("{USAGE}");
         return;
     };
 
-    let bids = std::fs::File::open(&format!("../nexmark-data/bid/bids.csv")).map(iter::<Bid>);
-    // let auctions = std::fs::File::open(&format!("data/auctions.csv")).map(iter::<Auction>);
-    // let persons = std::fs::File::open(&format!("data/persons.csv")).map(iter::<Person>);
+    let bids = std::fs::File::open(&format!("{dir}/bids.csv")).map(iter::<Bid>);
+    let auctions = std::fs::File::open(&format!("{dir}/auctions.csv")).map(iter::<Auction>);
+    let persons = std::fs::File::open(&format!("{dir}/persons.csv")).map(iter::<Person>);
 
     match query.as_str() {
         "q1" => timed(move |ctx| q1::run(stream(ctx, bids), ctx)),
+        "q2" => timed(move |ctx| q2::run(stream(ctx, bids), ctx)),
+        "q3" => timed(move |ctx| q3::run(stream(ctx, auctions), stream(ctx, persons), ctx)),
+        // "q4" => timed(move |ctx| q4::run(stream(ctx, auctions), stream(ctx, bids), ctx)),
+        // "q5" => timed(move |ctx| q5::run(stream(ctx, bids), ctx)),
+        // "q6" => timed(move |ctx| q6::run(stream(ctx, auctions), stream(ctx, bids), ctx)),
+        // "q7" => timed(move |ctx| q7::run(stream(ctx, bids), ctx)),
+        // "q8" => timed(move |ctx| q8::run(stream(ctx, auctions), stream(ctx, persons), ctx)),
+        // "qw" => {
+        //     let Some(size) = args.next() else {
+        //         println!("{USAGE} <size> <step>");
+        //         return;
+        //     };
+        //     let Some(step) = args.next() else {
+        //         println!("{USAGE} <size> <step>");
+        //         return;
+        //     };
+        //     let size = size.parse().unwrap();
+        //     let step = step.parse().unwrap();
+        //     timed(move |ctx| qw::run(stream(ctx, bids), size, step, ctx))
+        // },
+        // Optimised
         "q1-opt" => timed(move |ctx| q1::run_opt(stream(ctx, bids), ctx)),
-        "io" => timed(move |ctx| stream(ctx, bids).drain(ctx)),
+        "q2-opt" => timed(move |ctx| q2::run_opt(stream(ctx, bids), ctx)),
+        "q3-opt" => timed(move |ctx| q3::run_opt(stream(ctx, auctions), stream(ctx, persons), ctx)),
+        // "q4-opt" => timed(move |ctx| q4::run_opt(stream(ctx, auctions), stream(ctx, bids), ctx)),
+        // "q5-opt" => timed(move |ctx| q5::run_opt(stream(ctx, bids), ctx)),
+        // "q6-opt" => timed(move |ctx| q6::run_opt(stream(ctx, auctions), stream(ctx, bids), ctx)),
+        // "q7-opt" => timed(move |ctx| q7::run_opt(stream(ctx, bids), ctx)),
+        // "q8-opt" => timed(move |ctx| q8::run_opt(stream(ctx, auctions), stream(ctx, persons), ctx)),
+        // "qw-opt" => {
+        //     let size = args.next().unwrap().parse().unwrap();
+        //     let step = args.next().unwrap().parse().unwrap();
+        //     timed(move |ctx| qw::run_opt(stream(ctx, bids), size, step, ctx))
+        // },
+        
+        "io" => {
+            timed(move |ctx| {
+                if bids.is_ok() {
+                    stream(ctx, bids).drain(ctx);
+                }
+                if persons.is_ok() {
+                    stream(ctx, persons).drain(ctx);
+                }
+                if auctions.is_ok() {
+                    stream(ctx, auctions).drain(ctx);
+                }
+            });
+        },
         _ => panic!("unknown query"),
     }
 }
